a3fb6b82bac364489767fce4595bd8e51434fded
Fahmbz <fahmbz@git.com>
2020-03-22 05:18:55 +0700

    update 4.1

---
+++ app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/CalendatActivity.kt
@@ -0,0 +1,11 @@
+package com.th.ac.kku.cis.mobileapp.calclatorperiod
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalendatActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
--- app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/MainActivity.kt
+++ app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar)
+        setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
 
