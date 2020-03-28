c87206847be6a3ba9e886f2ec8778e9850c11f88
Fahmbz <fahmbz@git.com>
2020-03-29 05:22:37 +0700

    update 6

--- app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/AddEventActivity.kt
+++ app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/AddEventActivity.kt
@@ -1,12 +1,44 @@
 package com.th.ac.kku.cis.mobileapp.calclatorperiod
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_add_event.*
 
 class AddEventActivity : AppCompatActivity() {
+    var name_event : EditText? = null
+    var add_location : EditText? = null
+    var edit_detail : EditText? = null
+    var button_save_event : Button? = null
+    var dateset : EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_event)
 
+        name_event = findViewById(R.id.name_event)
+        add_location = findViewById(R.id.add_location)
+        edit_detail = findViewById(R.id.edit_detail)
+        dateset = findViewById(R.id.dateset)
+        button_save_event = findViewById(R.id.button_save_event)
+
+        button_save_event!!.setOnClickListener {
+            intent = Intent(this, DetailEventCalendar::class.java)
+            intent.putExtra("NAME",""+ name_event!!.getText().toString())
+            intent.putExtra("LOCATION",""+ add_location!!.getText().toString())
+            intent.putExtra("DETAIL",""+ edit_detail!!.getText().toString())
+            intent.putExtra("DATE",""+dateset!!.getText().toString())
+            startActivity(intent)
+
+
+        }
     }
 }
--- app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/CalendarActivity.kt
+++ app/src/main/java/com/th/ac/kku/cis/mobileapp/calclatorperiod/CalendarActivity.kt
@@ -3,31 +3,35 @@ package com.th.ac.kku.cis.mobileapp.calclatorperiod
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.CalendarView
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_about.*
 import kotlinx.android.synthetic.main.activity_calendar.*
-import java.util.*
 
 
 class CalendarActivity : AppCompatActivity () {
+
+    var calendar: CalendarView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        calendar = findViewById(R.id.calendarView)
+
         button_add.setOnClickListener {
-            var i = Intent(this,AddEventActivity::class.java)
+            var i = Intent(this, AddEventActivity::class.java)
             i.setFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP))
             startActivity(i)
         }
         button_about.setOnClickListener {
-            var i = Intent(this,AboutActivity::class.java)
+            var i = Intent(this, AboutActivity::class.java)
             i.setFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP))
             startActivity(i)
         }
+        button_detail.setOnClickListener { var i = Intent(this, DetailEventCalendar::class.java)
+            i.setFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            startActivity(i) }
     }
 }
